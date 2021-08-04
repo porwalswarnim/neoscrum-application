9769b257660dd5b72359045ffd1e7c2280a1badf
porwalswarnim <swarnim.porwal@neosoftmail.com>
2021-08-04 13:59:54 +0530

    first commit

---
+++ src/components/registration/RegistrationPage.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import "./RegistrationPage.css";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const RegistrationPage = (props) => {
+  const [employeeName, setEmployeeName] = useState("");
+  const [email, setEmail] = useState("");
+
+  const submitFormHandler = (e) => {
+    e.preventDefault();
+    console.log('e', e)
+    console.log('email', email)
+    console.log('employeeName', employeeName)
+  };
+  return (
+    <form onSubmit={submitFormHandler} className="registrationForm">
+      <h1>Enter New Developer</h1>
+      <input
+        type="text"
+        className="employeeName"
+        value={employeeName}
+        placeholder="Employee Name *"
+        onChange={(e) => setEmployeeName(e.target.value)}
+      ></input>
+      <hr />
+      <br />
+      <input
+        type="email"
+        value ={email}
+        className="emailbox"
+        placeholder="Email *"
+        onChange={(e) => setEmail(e.target.value)}
+      ></input>
+      <hr />
+      <input type="file" className="fileUpload" />
+      <br />
+      <button className="btn-primary" type="submit" value="Submit">
+        Submit
+      </button>
+      <hr />
+    </form>
+  );
+};
+
+export default RegistrationPage;
